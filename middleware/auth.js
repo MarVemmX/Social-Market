d22f17516e4a6e8d6e372263708157ec71243a6a
Thanh Tuan <tuango.472000@gmail.com>
2021-09-13 18:20:37 +0700

    done forgot password

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,34 +1,32 @@
-const jwt = require("jsonwebtoken");
-const ErrorResponse = require("../utils/errorResponse");
-const User = require("../models/User");
+import jwt from 'jsonwebtoken';
+import ErrorResponse from '../utils/errorResponse';
+import User from '../models/User';
 
+// middleware project route when go to home page
 exports.protect = async (req, res, next) => {
-  let token;
+    let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
-  }
-
-  if (!token) {
-    return next(new ErrorResponse("Not authorized to access this route", 401));
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    const user = await User.findById(decoded.id);
-
-    if (!user) {
-      return next(new ErrorResponse("No user found with this id", 404));
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
     }
 
-    req.user = user;
+    if (!token) {
+        return next(new ErrorResponse('Đăng nhập không hợp lệ', 401));
+    }
 
-    next();
-  } catch (err) {
-    return next(new ErrorResponse("Not authorized to access this router", 401));
-  }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findById(decoded.id);
+
+        if (!user) {
+            return next(new ErrorResponse('Not Found', 404));
+        }
+
+        req.user = user;
+
+        next();
+    } catch (err) {
+        return next(new ErrorResponse('', 401));
+    }
 };
