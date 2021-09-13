317ca2f26ce2f491f7ec91c19f00a8bebe311b45
Thanh Tuan <tuango.472000@gmail.com>
2021-09-13 18:19:00 +0700

    middleware login before rendering home page

--- controllers/private.js
+++ controllers/private.js
@@ -1,8 +1,6 @@
 exports.getPrivateRoute = (req, res, next) => {
-  res
-    .status(200)
-    .json({
-      success: true,
-      data: "You got access to the private data in this route",
+    res.status(200).json({
+        success: true,
+        data: 'Render ra home page',
     });
 };
