41630cb563ae276364eda9f7d4d8e20e74fccce3
Thanh Tuan <tuango.472000@gmail.com>
2021-09-17 21:48:21 +0700

    add default value for cloudinary_id

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,18 +2,20 @@ import nodemailer from 'nodemailer';
 
 const sendEmail = (options) => {
     const transporter = nodemailer.createTransport({
-        service: process.env.EMAIL_SERVICE,
+        // service: process.env.EMAIL_SERVICE,
+        host: 'smtp.gmail.com',
+        port: 587,
         auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD,
+            user: 'nguyendandinh472000@gmail.com', // process.env.EMAIL_USERNAME
+            pass: 'Aq1sw2de3', // process.env.EMAIL_PASSWORD
         },
     });
 
     const mailOptions = {
-        from: process.env.EMAIL_FROM,
-        to: options.to,
-        subject: options.subject,
-        html: options.text,
+        from: 'nguyendandinh472000@gmail.com', // process.env.EMAIL_FROM
+        to: options.to, // options.to: địa chỉ gởi đến
+        subject: 'abcTest', // options.subject: tiêu đề của mail
+        html: 'đây là nội dung test', // options.text: nội dung gửi
     };
 
     transporter.sendMail(mailOptions, function (err, info) {
