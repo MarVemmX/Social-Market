a8dd7d1beea25f646374a3b4945c6e0ec126b076
thanhtuan472k <tuango.472000@gmail.com>
2021-11-29 16:29:58 +0700

    define category model

---
+++ models/Category.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import slug from 'mongoose-slug-generator';
+
+const categorySchema = new mongoose.Schema(
+    {
+        nameCategory: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+        },
+        slug: {
+            type: String,
+            slug: 'nameCategory',
+            unique: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// Add plugins
+mongoose.plugin(slug);
+
+const Category = mongoose.model('Category', categorySchema);
+
+module.exports = Category;
