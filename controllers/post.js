91bd32492093d8320bfce146b7657a58b0acfc5b
thanhtuan472k <tuango.472000@gmail.com>
2021-12-16 20:57:13 +0700

    feature/post

---
+++ controllers/post.js
@@ -0,0 +1,50 @@
+import Post from '../models/Post';
+
+/**
+ * @desc   Create new post --> [POST] /api/post/create
+ * @access  Private 
+ */
+exports.createNewPost = async (req, res) => {
+    //................
+    //................
+}
+
+/**
+ * @desc   Get posts --> [GET] /api/post/
+ * @access  Private
+ */
+exports.getPosts = async (req, res) => {
+    //................
+    //................
+}
+
+/**
+ * @desc   Update post --> [PATCH] /api/post/:id
+ * @access  Private
+ */
+exports.updatePost = async (req, res) => {
+    //................
+    //................
+}
+
+/**
+ * @desc   Delete post --> [GET] /api/post/:id
+ * @access  Private
+ */
+exports.deletePost = async (req, res) => {
+    //................
+    //................
+}
+
+/**
+ * @desc   Get post --> [GET] /api/post/:id
+ * @access  Private
+ */
+exports.getPost = async (req, res) => {
+    //................
+    //................
+}
+
+
+
+
