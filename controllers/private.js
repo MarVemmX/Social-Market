8b8b8d2d182c421a00eb03a84fc574c59c2bc9a7
thanhtuan472k <tuango.472000@gmail.com>
2021-12-18 20:34:56 +0700

    remove private.js

--- controllers/private.js
+++
@@ -1,6 +0,0 @@
-exports.getPrivateRoute = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        data: 'Render ra home page',
-    });
-};
--- routes/private.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getPrivateRoute } from '../controllers/private';
-import { auth } from '../middleware/auth'; // middleware login trước khi vào thẳng home page
-const router = express.Router();
-
-router.get('/', auth, getPrivateRoute);
-
-module.exports = router;
